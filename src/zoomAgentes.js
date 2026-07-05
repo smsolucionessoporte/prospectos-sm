fa0319371d4fad26421e522261c753a8d9187932
Marisol Rodríguez <rodriguezsmarisol@gmail.com>
2026-07-05 19:30:54 -0300

    feat: automatizar envío de link de Zoom y recordatorio por Chatwoot al confirmar prospecto

--- src/zoomAgentes.js
+++ src/zoomAgentes.js
@@ -1,4 +1,6 @@
-// zoomAgentes.js
+// src/zoomAgentes.js
 const AGENTE_ZOOM = {
-  6: 'smsoluciones.soporte@gmail.com'
+  6: 'smsoluciones.soporte@gmail.com',
 };
+
+module.exports = { AGENTE_ZOOM };
